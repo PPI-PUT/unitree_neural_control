0f82c98ba9d94a50506a3dd97e574d761ea508df
Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>
2023-12-21 03:25:25 +0100

    feat: add remaping
    Signed-off-by: Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>

--- launch/unitree_a1_neural_control.launch.py
+++ launch/unitree_a1_neural_control.launch.py
@@ -35,6 +35,11 @@ def launch_setup(context, *args, **kwargs):
         parameters=[
             param_path
         ],
+        remappings=[
+            ("~/input/state", LaunchConfiguration("input_state_name")),
+            ("~/output/command", LaunchConfiguration("output_cmd_name")),
+            ("~/input/cmd_vel", LaunchConfiguration("input_cmd_vel_name")),
+        ],
         output='screen',
         arguments=['--ros-args', '--log-level', 'info', '--enable-stdout-logs'],
     )
@@ -53,7 +58,9 @@ def generate_launch_description():
         )
 
     add_launch_arg('unitree_a1_neural_control_param_file', '')
-
+    add_launch_arg('input_state_name', '/unitree_a1_legged/state')
+    add_launch_arg('output_cmd_name', '/unitree_a1_legged/nn/cmd')
+    add_launch_arg('input_cmd_vel_name', '/unitree_a1_legged/cmd_vel')
     return LaunchDescription([
         *declared_arguments,
         OpaqueFunction(function=launch_setup)
